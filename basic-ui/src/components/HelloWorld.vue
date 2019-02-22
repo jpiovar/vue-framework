d8826e2db0e810110476c8d8b26b80c63dbd6072
jpiovar <jpiovar@sk.ibm.com>
2019-02-22 23:39:05 +0100

    used actions getters in helloworld

--- basic-ui/src/components/HelloWorld.vue
+++ basic-ui/src/components/HelloWorld.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="hello">
     <h1>{{ msg }} {{ modified }}</h1>
+    <p>{{ userProfile.name }} {{ userProfile.email }}</p>
+    <button type="button" @click="logoutClicked">logout</button>
+    <button type="button" @click="loginClicked">login</button>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import { namespace } from 'vuex-class';
+import { USER } from '@/stores/constants';
+import { UserData } from '@/stores/user/user.types';
+
+const UserStore = namespace(USER);
 
 @Component({
   components: {},
@@ -19,8 +27,27 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 export default class HelloWorld extends Vue {
   msg!: string;
 
+  @UserStore.Action logoutUser!: () => void;
+
+  @UserStore.Action loginUser!: ({ id, name, email }: UserData) => void;
+
+  @UserStore.Getter userData!: UserData;
+
   get modified(): string {
     return `${this.msg} modifiedapp`;
+  }
+
+  get userProfile() {
+    const { name, email } = this.userData;
+    return { name, email };
+  }
+
+  logoutClicked() {
+    this.logoutUser();
+  }
+
+  loginClicked() {
+    this.loginUser({ id: 'idUser', name: 'nameUser', email: 'emailUser' });
   }
 }
 </script>
