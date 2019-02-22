3f953b77692174599bcb11e913621b284d3a571e
jpiovar <jpiovar@sk.ibm.com>
2019-02-22 23:19:34 +0100

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }} {{ modified }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {},
+  props: {
+    msg: {
+      required: true,
+      type: String,
+    },
+  },
+})
+export default class HelloWorld extends Vue {
+  msg!: string;
+
+  get modified(): string {
+    return `${this.msg} modifiedapp`;
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  margin: 40px 0 0;
+}
+</style>
